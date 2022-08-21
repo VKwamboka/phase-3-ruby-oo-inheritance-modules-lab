ef80a0b495d12914a80c422e9feeb5738ec8e262
verah <verahnyagaka101@gmail.com>
2022-08-21 09:43:40 +0300

    completed

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,8 @@ require_relative '../lib/concerns/memorable'
 class Song
 
   extend Memorable
+  extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -14,9 +16,9 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@songs
@@ -26,7 +28,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
