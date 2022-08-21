5692d307206ab8e44c9b7cc5bf27bb06a1c7479a
verah <verahnyagaka101@gmail.com>
2022-08-21 09:43:30 +0300

    commit tree 4833845be58a7e1e94bff85403a847daca86ca52

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,9 +4,11 @@ require_relative '../lib/concerns/memorable'
 class Artist
 
   extend Memorable
+  extend Findable
 
   attr_accessor :name
   attr_reader :songs
+  include Paramable
 
   @@artists = []
 
@@ -15,9 +17,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
@@ -32,7 +34,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
